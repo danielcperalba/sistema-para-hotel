36e511e2bf58643da1d2b4d6db2b29f7f400f27d
Daniel Peralba <contatodanielperalba@hotmail.com>
2023-12-16 10:33:17 -0300

    Add project files.

---
+++ Sistema_Hotel/Sistema_Hotel_Penedo/Controllers/QuartosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sistema_Hotel_Penedo.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sistema_Hotel_Penedo.Controllers
+{
+    [Route("api/quartos")]
+    public class QuartosController : ControllerBase
+    {
+        //api/quartos
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        //api/quartos/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CadastrarQuartoModel cadastrarQuarto)
+        {
+            if(cadastrarQuarto.QuantidadeHospedes < 1)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction(nameof(GetById), new { id = cadastrarQuarto.Id }, cadastrarQuarto);
+        }
+
+        //api/quartos/1
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]  )
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Sistema_Hotel/Sistema_Hotel_Penedo/Models/CadastrarQuartoModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Sistema_Hotel_Penedo.Models
+{
+    public class CadastrarQuartoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int QuantidadeHospedes { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public string Servicos { get; set; }
+        public string Status { get; set; }
+        public int Dimensao { get; set; }
+        public int Camas { get; set; }
+        public string Comidades { get; set; }
+
+    }
+}
